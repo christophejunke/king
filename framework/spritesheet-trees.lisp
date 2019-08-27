d5ba92ebe4fbc09c3bba7d27c6390c68dadfae94
Christophe Junke <junke.christophe@gmail.com>
2019-08-27 13:54:30 +0200

    Secondary value

--- framework/spritesheet-trees.lisp
+++ framework/spritesheet-trees.lisp
@@ -346,27 +346,35 @@ closure, w.r.t. to KEY and TEST, we increment a counter and affect it
 to the value. If however the value was already present, we return the
 existing index.
 
-Finally, when no argument is given, the closure returns a vector of
-all the given already seen by the closure, stored in the same order as
-the index which was associated with the value. Note that those values
-are the one being that were passed to the function, not the
+Finally, when no argument is given, the closure returns a fresh vector
+of all the given already seen by the closure, stored in the same order
+as the index which was associated with the value. Note that those
+values are the one being that were passed to the function, not the
 keys (which are used only for hashing)."
   (let ((map (make-hash-table :test test))
         (counter -1))
-    (lambda (&optional (data nil datap))
-      (if datap
-          ;; Store data
-          (let ((key (funcall key data)))
+    (values
+     (lambda (&optional (data nil datap))
+       (if datap
+           ;; Store data
+           (let ((key (funcall key data)))
              (cdr
               (or (gethash key map)
                   (setf (gethash key map)
                         (cons data (incf counter))))))
-          ;; Return index
-          (let ((array (make-array (hash-table-count map) :adjustable nil)))
-            (maphash (lambda (k v)
-                       (declare (ignore  k))
-                       (setf (aref array (cdr v)) (car v))) map)
-            array)))))
+           ;; Return index
+           (let ((array (make-array (hash-table-count map) :adjustable nil)))
+             (maphash (lambda (k v)
+                        (declare (ignore  k))
+                        (setf (aref array (cdr v)) (car v))) map)
+             array)))
+     ;; secondary value, compute vector of keys instead
+     (lambda ()
+       (let ((array (make-array (hash-table-count map) :adjustable nil)))
+         (maphash (lambda (k v)
+                    (setf (aref array (cdr v)) k))
+                  map)
+         array)))))
 
 ;; (defun test-indexer ()
 ;;   (let ((indexer (make-indexer :key #'namestring)))
